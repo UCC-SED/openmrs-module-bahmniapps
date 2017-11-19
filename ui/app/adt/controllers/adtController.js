e1a083387dbb480896ee937363bd51f86acc554a
focus <focus.medard@gmail.com>
2017-11-19 13:49:44 +0300

    focus shao| ADT changes

--- ui/app/adt/controllers/adtController.js
+++ ui/app/adt/controllers/adtController.js
@@ -3,10 +3,10 @@
 angular.module('bahmni.adt')
     .controller('AdtController', ['$scope', '$q', '$rootScope', 'spinner', 'dispositionService',
         'encounterService', 'bedService', 'appService', 'visitService', '$location', '$window', 'sessionService',
-        'messagingService', '$anchorScroll', '$stateParams', 'ngDialog', '$filter',
+        'messagingService', '$anchorScroll', '$stateParams', 'ngDialog', '$filter', 'configurationService', 'auditLogService',
         function ($scope, $q, $rootScope, spinner, dispositionService, encounterService, bedService,
                   appService, visitService, $location, $window, sessionService, messagingService, $anchorScroll,
-                  $stateParams, ngDialog, $filter) {
+                  $stateParams, ngDialog, $filter, configurationService, auditLogService) {
             var actionConfigs = {};
             var encounterConfig = $rootScope.encounterConfig;
             var locationUuid = sessionService.getLoginLocationUuid();
@@ -108,7 +108,6 @@ angular.module('bahmni.adt')
                 $scope.visitControl = new Bahmni.Common.VisitControl(visitTypes, defaultVisitType, visitService);
                 $scope.dashboard = Bahmni.Common.DisplayControl.Dashboard.create($scope.dashboardConfig || {}, $filter);
                 $scope.sectionGroups = $scope.dashboard.getSections($scope.diseaseTemplates);
-
                 return getVisit().then(dispositionService.getDispositionActions).then(function (response) {
                     if (response.data && response.data.results && response.data.results.length) {
                         $scope.dispositionActions = getDispositionActions(response.data.results[0].answers);
@@ -226,12 +225,28 @@ angular.module('bahmni.adt')
                 ngDialog.close();
             };
 
+            var log = function (patientUuid, visitStatus) {
+                configurationService.getConfigurations(['enableAuditLog']).then(function (result) {
+                    if (result.enableAuditLog) {
+                        var params = {};
+                        params.patientUuid = patientUuid;
+                        params.eventType = Bahmni.Common.AuditLogEventDetails[visitStatus].eventType;
+                        params.message = Bahmni.Common.AuditLogEventDetails[visitStatus].message + '~' +
+                                        JSON.stringify({visitUuid: $scope.visitSummary.uuid, visitType: $scope.visitSummary.visitType});
+                        params.module = "adt";
+                        auditLogService.auditLog(params);
+                    }
+                });
+            };
+
             $scope.closeCurrentVisitAndStartNewVisit = function () {
                 if (defaultVisitTypeUuid !== null) {
                     var encounter = getEncounterData($scope.encounterConfig.getAdmissionEncounterTypeUuid(), defaultVisitTypeUuid);
                     visitService.endVisitAndCreateEncounter($scope.visitSummary.uuid, encounterService.buildEncounter(encounter)).success(function (response) {
+                        log(encounter.patientUuid, "CLOSE_VISIT");
                         visitService.getVisitSummary(response.visitUuid).then(function (response) {
                             $scope.visitSummary = new Bahmni.Common.VisitSummary(response.data);
+                            log(encounter.patientUuid, "OPEN_VISIT");
                         });
                         forwardUrl(response, "onAdmissionForwardTo");
                     });
